703ae1a9d889a6d4069f05e990797694fcdd77dc
Yuting Wang <1810306210@pku.edu.cn>
2025-05-18 16:41:55 +0800

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,4 @@
 .onAttach <- function(libname, pkgname) {
 
-  options(
-    download.file.method.GEOquery = "auto",
-    GEOquery.inmemory.gpl = FALSE
-  )
-
   packageStartupMessage("Welcome! Thank you for using the R package easyEWAS! Detailed guidance can be found at https://easyewas.github.io/")
 }
