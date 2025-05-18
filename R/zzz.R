a0007dbced044d342308f6a6a967f6321ff5fa6b
Yuting Wang <1810306210@pku.edu.cn>
2025-05-18 17:32:27 +0800

    zzz

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,11 @@
 .onAttach <- function(libname, pkgname) {
+  
+  suppressMessages({
+    options('download.file.method.GEOquery' = 'auto')
+    options('GEOquery.inmemory.gpl' = FALSE)
+  })
 
-  packageStartupMessage("Welcome! Thank you for using the R package easyEWAS! Detailed guidance can be found at https://easyewas.github.io/")
+  packageStartupMessage("Welcome! Thank you for using the R package easyEWAS! Detailed guidance can be found at https://easyewas.github.io/.\n
+                        Please cite us:\n
+                        Wang Y, Jiang M, Niu S, Gao X. easyEWAS: a flexible and user-friendly R package for epigenome-wide association study[J]. Bioinformatics Advances, 2025, 5(1): vbaf026.")
 }
