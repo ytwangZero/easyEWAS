fe94b055af9063bba960c12b89c9504ea163b5ff
Yuting Wang <1810306210@pku.edu.cn>
2025-05-19 10:15:22 +0800

    jjj

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,6 @@
     "||__|||__|||__|||__|||__|||__|||__|||__||\n",
     "|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|\n",
     "\n",
-    "Welcome! Thank you for using the R package easyEWAS! \nDetailed guidance can be found at 'https://easyewas.github.io/'.\nPlease cite us:Wang Y, Jiang M, Niu S, Gao X. easyEWAS: a flexible and user-friendly R package for epigenome-wide association study[J]. Bioinformatics Advances, 2025, 5(1): vbaf026.")
+    "Welcome! Thank you for using the R package easyEWAS! \nDetailed guidance can be found at 'https://easyewas.github.io/'.\nPlease cite us: Wang Y, Jiang M, Niu S, Gao X. easyEWAS: a flexible and user-friendly R package for epigenome-wide association study[J]. Bioinformatics Advances, 2025, 5(1): vbaf026.")
 }
 
