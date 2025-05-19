800a8f9902be362632fe86005cd727a4b55fbdca
Yuting Wang <1810306210@pku.edu.cn>
2025-05-19 10:10:52 +0800

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,11 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome! Thank you for using the R package easyEWAS! Detailed guidance can be found at https://easyewas.github.io/.\nPlease cite us:\nWang Y, Jiang M, Niu S, Gao X. easyEWAS: a flexible and user-friendly R package for epigenome-wide association study[J]. Bioinformatics Advances, 2025, 5(1): vbaf026.")
+  packageStartupMessage(
+    "\n",
+    " ____ ____ ____ ____ ____ ____ ____ ____ \n",
+    "||E |||A |||S |||Y |||E |||W |||A |||S ||\n",
+    "||__|||__|||__|||__|||__|||__|||__|||__||\n",
+    "|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|\n",
+    "\n",
+    "Welcome! Thank you for using the R package easyEWAS! \nDetailed guidance can be found at 'https://easyewas.github.io/'.\nPlease cite us:Wang Y, Jiang M, Niu S, Gao X. easyEWAS: a flexible and user-friendly R package for epigenome-wide association study[J]. Bioinformatics Advances, 2025, 5(1): vbaf026.")
 }
+
