ef987251c35aeee45e5dea8cbe11ac19272277c8
Yuting Wang <1810306210@pku.edu.cn>
2025-05-19 16:28:34 +0800

    eee

--- R/zzz.R
+++ R/zzz.R
@@ -10,7 +10,7 @@
     "╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝ ╚══╝╚══╝ ╚═╝  ╚═╝╚══════╝\n",
     "\n",
     "Authors: Yuting Wang, Xu Gao (Corresponding author).\n",
-    "Documentation:https://easyewas.github.io/.\n",
+    "Documentation: https://easyewas.github.io/.\n",
     "Citation: Wang Y, Jiang M, Niu S, Gao X. easyEWAS: a flexible and user-friendly R package for epigenome-wide association study[J]. Bioinformatics Advances, 2025, 5(1): vbaf026.")
 }
 
