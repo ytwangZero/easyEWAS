113eaae4a6fa909faa89fdb2f51b717d3a65d5fb
Yuting Wang <ytwangzero@outlook.com>
2026-03-03 11:31:24 +0800

    fix bugs

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,15 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage(
-    "Welcome! Thank you for using the R package easyEWAS! \n",
-    "Authors: Yuting Wang, Xu Gao (Corresponding author).\n",
-    "Documentation: https://ytwangZero.github.io/easyEWAS/.\n",
-    "Citation: Wang Y, Jiang M, Niu S, Gao X. easyEWAS: a flexible and user-friendly R package for epigenome-wide association study[J]. Bioinformatics Advances, 2025, 5(1): vbaf026.")
+
+  version <- utils::packageVersion(pkgname)
+
+  msg <- paste0(
+    "Welcome to easyEWAS ", version, "!\n",
+    strrep("=", 24 + nchar(as.character(version))), "\n",
+    "Flexible and user-friendly tools for Epigenome-Wide Association Studies.\n\n",
+    "Authors: Yuting Wang & Xu Gao (Corresponding author)\n",
+    "Documentation: https://easyewas-tutorial.github.io/\n",
+    "Citation:https://doi.org/10.1093/bioadv/vbaf026\n"
+  )
+
+  packageStartupMessage(msg)
 }
